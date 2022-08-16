b99a4481e36755f79559dc50d6c28139bd97b93e
Thalita <thalitaaesser@gmail.com>
2022-08-16 19:07:16 -0400

    update of .js file

--- main.js
+++ main.js
@@ -50,6 +50,7 @@ function createTagLi(task)
 {
     let li = document.createElement('li');
     li.classList.add('tasks');
+    li.id = task.id;
     let span = document.createElement('span');
     span.classList.add('task-box');
     span.innerHTML = task.name;
@@ -58,15 +59,34 @@ function createTagLi(task)
     let btnEdit = document.createElement('button');
     btnEdit.classList.add('edit-task-btn');
     btnEdit.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#5b3a92" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"/> <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /> <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /> <line x1="16" y1="5" x2="19" y2="8" /> </svg>';
-
+    btnEdit.setAttribute('onclick','editTask('+task.id+')');
     let btnDel = document.createElement('button');
     btnDel.classList.add('del-task-btn');
     btnDel.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#5b3a92" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"/> <line x1="4" y1="7" x2="20" y2="7" /> <line x1="10" y1="11" x2="10" y2="17" /> <line x1="14" y1="11" x2="14" y2="17" /> <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /> <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>';
-
+    btnDel.setAttribute('onclick','delTask('+task.id+')');
     div.appendChild(btnEdit);
     div.appendChild(btnDel);
     li.appendChild(span);
     li.appendChild(div);
     return li;
 }
+function delTask(idTask)
+{
+    let confirm = window.confirm("!! Atention !!\nAre you sure that you wanna delete this task?")
+    if(confirm)
+    {
+        let task = document.getElementById(''+idTask+'');
+        if(task)
+        {
+            list_tasks.removeChild(task);
+        }
+    }
+}
+function editTask(idTask)
+{
+    let update = document.getElementById(''+idTask+'');
+    if (update)
+    {
 
+    }
+}
